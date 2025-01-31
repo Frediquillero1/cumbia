241e85d25618c747c7aa73676d1f6d60d455134c
Fred Emilio Schlesinger <fesgo@rocketmail.com>
2025-01-31 23:32:48 +0100

    create-checkout-page

--- lib/validator.ts
+++ lib/validator.ts
@@ -65,16 +65,27 @@ export const OrderItemSchema = z.object({
   color: z.string().optional(),
 });
 
+// Cart
+export const ShippingAddressSchema = z.object({
+  fullName: z.string().min(1, 'Full name is required'),
+  street: z.string().min(1, 'Address is required'),
+  city: z.string().min(1, 'City is required'),
+  postalCode: z.string().min(1, 'Postal code is required'),
+  province: z.string().min(1, 'Province is required'),
+  phone: z.string().min(1, 'Phone number is required'),
+  country: z.string().min(1, 'Country is required'),
+});
+
 export const CartSchema = z.object({
   items: z
     .array(OrderItemSchema)
     .min(1, 'Order must contain at least one item'),
   itemsPrice: z.number(),
-
   taxPrice: z.optional(z.number()),
   shippingPrice: z.optional(z.number()),
   totalPrice: z.number(),
   paymentMethod: z.optional(z.string()),
+  shippingAddress: z.optional(ShippingAddressSchema),
   deliveryDateIndex: z.optional(z.number()),
   expectedDeliveryDate: z.optional(z.date()),
 });
@@ -83,10 +94,10 @@ export const CartSchema = z.object({
 const UserName = z
   .string()
   .min(2, { message: 'Username must be at least 2 characters' })
-  .max(50, { message: 'Username must be at most 30 characters' })
-const Email = z.string().min(1, 'Email is required').email('Email is invalid')
-const Password = z.string().min(3, 'Password must be at least 3 characters')
-const UserRole = z.string().min(1, 'role is required')
+  .max(50, { message: 'Username must be at most 30 characters' });
+const Email = z.string().min(1, 'Email is required').email('Email is invalid');
+const Password = z.string().min(3, 'Password must be at least 3 characters');
+const UserRole = z.string().min(1, 'role is required');
 
 export const UserInputSchema = z.object({
   name: UserName,
@@ -105,16 +116,16 @@ export const UserInputSchema = z.object({
     country: z.string().min(1, 'Country is required'),
     phone: z.string().min(1, 'Phone number is required'),
   }),
-})
+});
 
 export const UserSignInSchema = z.object({
   email: Email,
   password: Password,
-})
+});
 export const UserSignUpSchema = UserSignInSchema.extend({
   name: UserName,
   confirmPassword: Password,
 }).refine((data) => data.password === data.confirmPassword, {
   message: "Passwords don't match",
   path: ['confirmPassword'],
-})
+});
